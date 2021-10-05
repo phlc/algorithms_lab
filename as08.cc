662554bee555ce89460764689bd529307e5f2479
phlc <pedrocarvalho@outlook.com>
2021-10-05 13:41:55 -0300

    as08.cc Complexity Analisys OK

--- as08.cc
+++ as08.cc
@@ -7,9 +7,13 @@ Matricula: 651230
 AS08
 
 Análise:
-         
-        
+         Algoritmo calcula o número de movimentos de um valor do arranjo para a esquerda quando da ordenação.
+         Utiliza-se o mergeSort com a contagem desses movimentos. O mergeSort a cada nível de recursão, fase de divisão,
+         divide o problema em 2. Na volta da recursão, merge, em cada nível da recursão, realiza n comparações. 
+         Assim, o algoritmo tem complexidade O(n Lg n), sendo n o tamanho do arranjo a ser ordenado.
 */
+
+
 //Macros
 #define MAX 0x7ffffff
 
@@ -41,20 +45,20 @@ int* mergeSort(int arr[], int i, int j, int& cont){
         int* esq;
         int* dir;
         int k = (j+i)/2;
+        int i_esq = 0; //indice esquerda
+        int i_dir = 0; //indice direita
 
         //Chamada recursiva
         esq = mergeSort(arr, i, k, cont);
         dir = mergeSort(arr, k+1, j, cont);
 
+        
         //Merge
-        int i_esq = 0; //indice esquerda
-        int i_dir = 0; //indice direita
-
         for(int i_resultado = 0; i_resultado<tamanho-1; i_resultado++){
             if(esq[i_esq] > dir[i_dir]){
                 resultado[i_resultado] = dir[i_dir];
                 i_dir++;
-                cont += k-i+1-i_esq; //quantos movimetacoes dir[i_dir] fez
+                cont += k-i+1-i_esq; //quantos movimetacoes dir[i_dir] fez para esquerda
             }
             else{
                 resultado[i_resultado] = esq[i_esq];
